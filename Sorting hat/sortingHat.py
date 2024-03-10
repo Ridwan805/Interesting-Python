4997a6414a9706d87df8207a6ba0ddda25c2b75c
Ridwan805 <ridwanurrahman214@gmail.com>
2024-03-10 11:34:09 +0600

    Added 2 more question and made each function indivdual

--- Sorting hat/sortingHat.py
+++ Sorting hat/sortingHat.py
@@ -5,7 +5,44 @@ window = Tk()
 window.title("Sorting hat")
 window.geometry("900x400")
 
+def intro_next():
 
+  global house
+  global letsgo
+
+  global intro
+  global intro_button
+  
+  intro.destroy()
+  intro_button.destroy()
+
+  house = Label(window, text = "Let's put you in a house\nWith the Sorting Hat",font=("Times new Roman",25))
+  house.pack()
+  letsgo = Button(window, text= "LESSGOOOO", padx= 40, pady=20,borderwidth=4,command= putting_house)
+  letsgo.pack()
+
+def putting_house():
+    global house
+    global letsgo
+
+    global question_1
+    global button_1
+    global button_2
+
+    global intro
+    global intro_button
+    
+    house.destroy()
+    letsgo.destroy()
+
+    question_1 = Label(window, text = "Q1) Do you like dawn or dusk?",font=("Times new Roman",25) )
+    question_1.pack()
+
+    button_1 = Button(window, text= "a) Dusk", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dusk"))
+    button_2 = Button(window, text= "b) Dawn", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dawn"))
+    button_1.pack()
+    button_2.pack()
+   
     
 def q1(n):
     
@@ -13,9 +50,16 @@ def q1(n):
     global sly
     global raven
     global huff
+
     global question_1
     global button_1
     global button_2
+
+    global question_2
+    global button_3
+    global button_4
+    global button_5
+    global button_6
     
     griff = 0
     sly = 0
@@ -42,119 +86,205 @@ def q1(n):
     button_5.pack()
     button_6.pack()
 
-    def q2(n):
-        global griff 
-        global sly
-        global raven
-        global huff
+def q2(n):
+    global griff 
+    global sly
+    global raven
+    global huff
 
-        if n == "good":
-          huff += 3
-        elif n == "great":
-          sly += 3
-        elif n == "wise":
-          raven += 3
-        elif n == "bold":
-          griff += 3
+    global question_2
+    global button_3
+    global button_4
+    global button_5
+    global button_6
 
-        question_2.destroy()
-        button_3.destroy()
-        button_4.destroy()
-        button_5.destroy()
-        button_6.destroy()
-        question_3 = Label(window, text = "Q3) Which kind of instrument most pleases your ear?",font=("Times new Roman",25) )
-        question_3.pack()
-        button_7 = Button(window, text= "1) The violin", padx= 45, pady=20,borderwidth=4,command=lambda: q3( "violin"))
-        button_8 = Button(window, text= "2) The trumpet", padx= 38, pady=20,borderwidth=4,command=lambda: q3( "trumpet"))
-        button_9 = Button(window, text= "3) The piano", padx= 44, pady=20,borderwidth=4,command=lambda: q3( "piano"))
-        button_10 = Button(window, text= "4) The drum", padx= 45, pady=20,borderwidth=4,command=lambda: q3( "drum"))
-        button_7.pack()
-        button_8.pack()
-        button_9.pack()
-        button_10.pack()
+    global question_3
+    global button_7
+    global button_8
+    global button_9
+    global button_10
 
-        def q3(n):
-            global griff 
-            global sly
-            global raven
-            global huff
-            global button_11
 
-            if n == "trumpet":
-                huff += 4
-            elif n == "violin":
-                sly += 4
-            elif n == "piano":
-                raven += 4
-            elif n == "drum":
-                griff += 4
-            question_3.destroy()
-            button_7.destroy()
-            button_8.destroy()
-            button_9.destroy()
-            button_10.destroy()
+    if n == "good":
+      huff += 3
+    elif n == "great":
+      sly += 3
+    elif n == "wise":
+      raven += 3
+    elif n == "bold":
+      griff += 3
 
-            question_4 = Label(window, text="What would like for an activity?",font=("Times new Roman",25))
-            question_4.pack()
-            button_13 = Button(window, text= "1) Working with plants", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "plants"))
-            button_14 = Button(window, text= "2) Strategic thinking", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "stra"))
-            button_15 = Button(window, text= "3) Going on an Adventure", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "adventure"))
-            button_16 = Button(window, text= "4) Solving puzzles", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "puzzles"))
-            button_13.pack()
-            button_14.pack()
-            button_15.pack()
-            button_16.pack()
-            
-            def q4(n):
-                global griff 
-                global sly
-                global raven
-                global huff
-                global button_11
-                global done
-                if n == "plants":
-                  huff += 3
-                elif n == "stra":
-                    sly += 3
-                elif n == "puzzles":
-                    raven += 3
-                elif n == "adventure":
-                    griff += 3
+    question_2.destroy()
+    button_3.destroy()
+    button_4.destroy()
+    button_5.destroy()
+    button_6.destroy()
+    question_3 = Label(window, text = "Q3) Which kind of instrument most pleases your ear?",font=("Times new Roman",25) )
+    question_3.pack()
+    button_7 = Button(window, text= "1) The violin", padx= 45, pady=20,borderwidth=4,command=lambda: q3( "violin"))
+    button_8 = Button(window, text= "2) The trumpet", padx= 38, pady=20,borderwidth=4,command=lambda: q3( "trumpet"))
+    button_9 = Button(window, text= "3) The piano", padx= 44, pady=20,borderwidth=4,command=lambda: q3( "piano"))
+    button_10 = Button(window, text= "4) The drum", padx= 45, pady=20,borderwidth=4,command=lambda: q3( "drum"))
+    button_7.pack()
+    button_8.pack()
+    button_9.pack()
+    button_10.pack()
 
-                question_4.destroy()
-                button_13.destroy()
-                button_14.destroy()
-                button_15.destroy()
-                button_16.destroy()
-                
-                question_5 = Label(window, text="Who was the founder of THE HOUSE HUFFLEPUFF?",font=("Times new Roman",25))
-                question_5.pack()
-                e = Entry(window, width= 60, border=5, borderwidth=4)
-            
-                e.pack()
-                x = e.get()
-                x.upper()    
-                button_17 = Button(window, text= "Next", padx= 45, pady=20,borderwidth=4,command=lambda: q5(x))
-                button_17.pack()
-                def q5(n):
-                  global griff 
-                  global sly
-                  global raven
-                  global huff
-                  global button_11
-                  global done 
-      
-                  if n == 'HELGA HUFFLEPUFF':
-                    huff += 10;
-                  else:
-                    huff -= 3
-                  e.destroy()
-                  question_5.destroy()
-                  button_17.destroy()
-                              
-                  button_11 = Button(window, text= "Who won?",font=("Times new roman", 25), padx= 60, pady=30,borderwidth=4,command= veri)
-                  button_11.place(x = 295, y = 140)
+def q3(n):
+    global griff 
+    global sly
+    global raven
+    global huff
+    global button_11
+
+    global question_4
+    global button_13
+    global button_14
+    global button_15
+    global button_16
+
+    global question_3
+    global button_7
+    global button_8
+    global button_9
+    global button_10
+
+    if n == "trumpet":
+        huff += 4
+    elif n == "violin":
+        sly += 4
+    elif n == "piano":
+        raven += 4
+    elif n == "drum":
+        griff += 4
+    question_3.destroy()
+    button_7.destroy()
+    button_8.destroy()
+    button_9.destroy()
+    button_10.destroy()
+
+    question_4 = Label(window, text="Q4) What would like for an activity?",font=("Times new Roman",25))
+    question_4.pack()
+    button_13 = Button(window, text= "1) Working with plants", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "plants"))
+    button_14 = Button(window, text= "2) Strategic thinking", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "stra"))
+    button_15 = Button(window, text= "3) Going on an Adventure", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "adventure"))
+    button_16 = Button(window, text= "4) Solving puzzles", padx= 45, pady=20,borderwidth=4,command=lambda: q4( "puzzles"))
+    button_13.pack()
+    button_14.pack()
+    button_15.pack()
+    button_16.pack()
         
+def q4(n):
+    global griff 
+    global sly
+    global raven
+    global huff
+
+    global question_4
+    global button_13
+    global button_14
+    global button_15
+    global button_16
+    
+    global question_5
+    global button_17
+    global e
+    
+
+    
+
+    if n == "plants":
+      huff += 3
+    elif n == "stra":
+        sly += 3
+    elif n == "puzzles":
+        raven += 3
+    elif n == "adventure":
+        griff += 3
+
+    question_4.destroy()
+    button_13.destroy()
+    button_14.destroy()
+    button_15.destroy()
+    button_16.destroy()
+    
+    question_5 = Label(window, text="Q5) Who was the founder of THE HOUSE HUFFLEPUFF?",font=("Times new Roman",25))
+    question_5.pack()
+    e = Entry(window, width= 60, border=5, borderwidth=4)
+    e.pack()
+    x = e.get()
+    x.upper()   
+
+    button_17 = Button(window, text= "Next", padx= 45, pady=20,borderwidth=4,command=lambda: q5(x))
+    button_17.pack()
+
+def q5(n):
+  global griff 
+  global sly
+  global raven
+  global huff
+  
+
+  global question_5
+  global button_17
+  global e
+
+  global question_6
+  global button_18
+  global button_19
+  global button_20
+  global button_21
+  
+  
+
+  if n == 'HELGA HUFFLEPUFF':
+    huff += 10;
+  else:
+    huff -= 3
+  e.destroy()
+  question_5.destroy()
+  button_17.destroy()
+  
+  question_6 = Label(window, text="Q6) Who is the ghost of Gryffindor?")
+  question_6.pack()
+
+  button_18 = Button(window, text= "1) Fat Friar", padx= 54, pady=20,borderwidth=4,command= lambda: q6("fat"))
+  button_19 = Button(window, text= "2) Bloody Baron", padx= 39, pady=20,borderwidth=4,command= lambda: q6('blood'))
+  button_20 = Button(window, text= "3) Nearly Headless Nick", padx= 20, pady=20,borderwidth=4,command= lambda: q6('headless'))
+  button_21 = Button(window, text= "4) The Grey Lady", padx= 40, pady=20,borderwidth=4,command= lambda: q6('grey'))
+
+  button_18.pack()
+  button_19.pack()
+  button_20.pack()
+  button_21.pack()
+
+def q6(n):
+  global griff 
+  global sly
+  global raven
+  global huff
+  global button_11
+
+  global question_6
+  global button_18
+  global button_19
+  global button_20
+  global button_21
+
+  if n == 'headless':
+    griff += 10;
+  else:
+    griff -= 3
+
+  question_6.destroy()
+  button_18.destroy()
+  button_19.destroy()
+  button_20.destroy()
+  button_21.destroy()
+   
+  button_11 = Button(window, text= "Who won?",font=("Times new roman", 25), padx= 60, pady=30,borderwidth=4,command= veri)
+  button_11.place(x = 295, y = 140)     
+
 def veri():
   global griff 
   global sly
@@ -182,31 +312,34 @@ def veri():
 def reset():
     global button_12
     global done
-    global question_1
-    global button_1
-    global button_2
+    global intro
+    global intro_button
+    
 
     done.destroy()
     button_12.destroy()
-    question_1 = Label(window, text = "Q1) Do you like dawn or dusk?",font=("Times new Roman",25) )
-    question_1.pack()
+    intro = Label(window, text = "Welcome to Howgwarts",font=("Times new Roman",25))
+    intro.pack()
+    intro_button = Button(window, text= "Next", padx= 40, pady=20,borderwidth=4,command=intro_next) 
+    intro_button.pack()
 
-    button_1 = Button(window, text= "a) Dusk", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dusk"))
-    button_2 = Button(window, text= "b) Dawn", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dawn"))
-    button_1.pack()
-    button_2.pack()
+    # question_1 = Label(window, text = "Q1) Do you like dawn or dusk?",font=("Times new Roman",25) )
+    # question_1.pack()
+
+    # button_1 = Button(window, text= "a) Dusk", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dusk"))
+    # button_2 = Button(window, text= "b) Dawn", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dawn"))
+    # button_1.pack()
+    # button_2.pack()
    
 
         
 
+intro = Label(window, text = "Welcome to Howgwarts",font=("Times new Roman",25))
+intro.pack()
+intro_button = Button(window, text= "Next", padx= 40, pady=20,borderwidth=4,command=intro_next) 
+intro_button.pack()
 
-question_1 = Label(window, text = "Q1) Do you like dawn or dusk?",font=("Times new Roman",25) )
-question_1.pack()
 
-button_1 = Button(window, text= "a) Dusk", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dusk"))
-button_2 = Button(window, text= "b) Dawn", padx= 40, pady=20,borderwidth=4,command=lambda: q1( "Dawn"))
-button_1.pack()
-button_2.pack()
 
 
 
