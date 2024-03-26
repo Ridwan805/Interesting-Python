ca1c72d2f93d249270346e1e68303d666985148e
ridwan805 <ridwanurrahman214@gmail.com>
2024-03-26 23:59:32 +0600

    Finished a the app

--- Image view app/viewer.py
+++ Image view app/viewer.py
@@ -13,4 +13,48 @@ img_lst = [img_1,img_2,img_3]
 my_label = Label(image=img_1)
 my_label.grid(row=0,column=0, columnspan=3)
 
+def forward(image):
+    global my_label
+    global button_next
+    global button_back
+    global img_lst
+
+    my_label.grid_forget()
+    my_label = Label(image=img_lst[image-1])
+    button_next = Button(window,text=">>", command=lambda: forward(image+1))
+    button_back = Button(window, text="<<",command=lambda: back(image-1))
+
+    if image == len(img_lst):
+        button_next = Button(window, text=">>", state=DISABLED)
+
+    button_back.grid(column=0,row=1)
+    button_next.grid(column=2,row=1)
+    my_label.grid(row=0,column=0, columnspan=3)
+
+
+def back(image):
+    global my_label
+    global button_next
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=img_lst[image-1])
+    button_next = Button(window,text=">>", command=lambda: forward(image+1))
+    button_back = Button(window, text="<<",command=lambda: back(image-1))
+    
+    if image == 1:
+        button_back = Button(window, text="<<", state=DISABLED)
+
+    button_back.grid(column=0,row=1)
+    button_next.grid(column=2,row=1)
+    my_label.grid(row=0,column=0, columnspan=3)
+
+
+button_back = Button(window, text="<<",command=back, state= DISABLED) 
+button_exit = Button(window,text="EXIT", command=window.quit)
+button_next = Button(window, text=">>",command=lambda: forward(2))
+
+button_back.grid(column=0,row=1)
+button_exit.grid(column=1,row=1)
+button_next.grid(column=2,row=1)
 window.mainloop()
